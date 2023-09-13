9b63fd649d981f0cbbea578376a64606fca290c4
danielvieira95 <engdanielvieira95@gmail.com>
2023-09-13 20:45:29 -0300

    exemplo criação de classes

--- Codigos/Aula08-TurmaB/exclasse.dart
+++ Codigos/Aula08-TurmaB/exclasse.dart
@@ -1,6 +1,21 @@
 // Criar classe chamada casa
 class Casa {
   String? cor; // Atributo cor
+  double? valor;
+
+// criar metodos
+  void abrirJanela(int qtde_janelas) {
+    print("Abrir janela, qtde de janelas $qtde_janelas");
+  }
+
+  void abrirPorta() {
+    print("Porta aberta");
+  }
+
+  void abrirCasa() {
+    this.abrirJanela(2);
+    this.abrirPorta();
+  }
 }
 
 void main() {
@@ -8,4 +23,12 @@ void main() {
   Casa minhaCasa = Casa();
   minhaCasa.cor = "Blue";
   print(minhaCasa.cor);
+  minhaCasa.abrirPorta();
+  minhaCasa.abrirJanela(3);
+  minhaCasa.abrirCasa();
+  Casa imovel1 = Casa();
+  imovel1.cor = "Vermelho";
+  print(imovel1.cor);
+  imovel1.valor = 550.000;
+  print("O valor do imovel 1 é R\$  ${imovel1.valor} ");
 }
