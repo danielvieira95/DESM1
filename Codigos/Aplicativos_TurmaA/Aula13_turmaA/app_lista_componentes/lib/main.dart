dd4c0d44077b11779be550603360153e7b943148
danielvieira95 <engdanielvieira95@gmail.com>
2023-10-27 22:21:21 -0300

    aplicativo lista de componentes
    - App Lista de componentes

--- Codigos/Aplicativos_TurmaA/Aula13_turmaA/app_lista_componentes/lib/main.dart
+++ Codigos/Aplicativos_TurmaA/Aula13_turmaA/app_lista_componentes/lib/main.dart
@@ -2,40 +2,69 @@ import 'package:flutter/material.dart'; // Permite criar os widgets para SO Andr
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: Scaffold(
       appBar: AppBar(
         title: Text("Lista de componentes"),
       ),
-      body: Column(
+      body: ListView(
         children: [
-          Stack(children: [
-            Container(
-              color: Colors.orange,
-              width: 400,
-              height: 150,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Container(
-                  color: Colors.blueGrey,
-                  height: 120,
-                  width: 100,
-                ),
-                Text(
-                  "Resistor",
-                  style: TextStyle(fontSize: 22),
-                ),
-              ],
-            ),
-          ]),
-          Container(
-            color: Colors.blue,
-            width: 400,
-            height: 20,
-          ),
+          ListaComponente('Resistor', 'assets/resistor.JPG'),
+          ListaComponente('Diodo', 'assets/diodo.JPG'),
+          ListaComponente('Led', 'assets/led.JPG'),
+          ListaComponente('Transistor', 'assets/transistor.JPG'),
+          ListaComponente('Capacitor', 'assets/capacitor.JPG'),
         ],
       ),
     ),
   ));
+}
+
+class ListaComponente extends StatelessWidget {
+  final String componente, imgc;
+  ListaComponente(this.componente, this.imgc,
+      {super.key}); // Construtor da classe
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Stack(children: [
+          Container(
+            color: Colors.orange,
+            width: 400,
+            height: 150,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                color: Colors.blueGrey,
+                height: 120,
+                width: 100,
+                child: Image.asset(
+                  imgc,
+                  fit: BoxFit.fill,
+                ), // Boxfit. Fill a image preenche todo container
+              ),
+              Text(
+                componente,
+                style: TextStyle(fontSize: 22),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    print("Nome do componente $componente");
+                  },
+                  child: Text("Exibir")),
+            ],
+          ),
+        ]),
+        Container(
+          color: Colors.blue,
+          width: 400,
+          height: 20,
+        ),
+      ],
+    );
+  }
 }
