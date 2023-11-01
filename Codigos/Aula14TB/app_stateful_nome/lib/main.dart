d8be669a0a9f9d0ce4f7787596fc0a22a5f3c8e3
danielvieira95 <engdanielvieira95@gmail.com>
2023-11-01 20:22:40 -0300

    app statefull
    Widget Statefull
    Botões

--- Codigos/Aula14TB/app_stateful_nome/lib/main.dart
+++ Codigos/Aula14TB/app_stateful_nome/lib/main.dart
@@ -5,10 +5,18 @@ void main() {
 }
 
 // Cria uma classe com um Widget personalizável do tipo Stateless
-class App_aula14 extends StatelessWidget {
+class App_aula14 extends StatefulWidget {
   // Construtor
   const App_aula14({super.key});
 
+  @override
+// State variavel de estado , essa variável que vai monitorar o estado app
+// para realizar alterações dinâmicas na tela
+  State<App_aula14> createState() => _App_aula14State();
+}
+
+class _App_aula14State extends State<App_aula14> {
+  String _msg = "MOBILE";
   @override // Polimorfismo permite sobreescrever os comandos das
   // funções
   // Widget build método para reconstruir a tela quando a
@@ -28,9 +36,36 @@ class App_aula14 extends StatelessWidget {
           children: [
             Container(
               color: Colors.blue,
-              width: 100,
-              height: 100,
+              width: 500, // Largura
+              height: 200, // altura
+              child: Text(
+                _msg,
+                style: TextStyle(fontSize: 28),
+                textAlign:
+                    TextAlign.center, // alinha texto no centro do container
+              ), // child estabelece
+              // relação com 1 elemento apenas
             ),
+            // Cria um botão na tela do aplicativo
+            // on pressed parametro que botão que vai chamar
+            // a ação a ser executada
+            // child estabele relação do botão com algo Text, Icon
+            ElevatedButton(
+                onPressed: () {
+                  print("Botão pressionado");
+                  setState(() {
+                    // função set state que irá alterar o estado do widget
+                    _msg = "SENAI"; // cria variavel msg
+                  });
+                },
+                child: Text("Mensagem")),
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _msg = "DESENVOLVIMENTO MOBILE 1"; // mensagem 2
+                  });
+                },
+                child: Text("Mensagem 2")),
           ],
         ),
       ),
